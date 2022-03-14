07caf64dfc0e3efd5ce602863f31114f9d4db97a
patgut <gutek100>
2022-03-14 16:31:57 +0100

    Create first sprites
    Create and test sprites that they work correctly

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,19 +1,39 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Game extends JFrame {
-    private int width = 400;
-    private int height = 400;
+
+    private static int width = 656;
+    private static int height = 646;
     private GameScreen gameScreen;
+    private BufferedImage image;
 
     public Game() {
+
+        importImage();
+
         setSize(width, height);
-        setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        gameScreen = new GameScreen();
+        gameScreen = new GameScreen(image);
         add(gameScreen);
+        setVisible(true);
+    }
+
+    private void importImage() {
+
+        InputStream is = getClass().getResourceAsStream("/res/spriteatlas.png");
+
+        try {
+            image = ImageIO.read(is);
+        } catch (IOException event) {
+            event.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -2,17 +2,47 @@ package main;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class GameScreen extends JPanel {
 
-    public GameScreen() {
+    private Random random;
+    private BufferedImage image;
 
+    private ArrayList<BufferedImage> sprites = new ArrayList<>();
+
+    public GameScreen(BufferedImage image) {
+        this.image = image;
+
+        loadSprites();
+
+        random = new Random();
+    }
+
+    private void loadSprites() {
+
+        for(int y = 0; y < 3; ++y) {
+            for(int x = 0; x < 10; ++x) {
+                sprites.add(image.getSubimage(x * 32, y * 32, 32, 32));
+            }
+        }
     }
 
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
 
-        graphics.setColor(new Color(90, 8, 24));
-        graphics.fillRect(50, 50, 100, 100);
+
+        for(int y = 0; y < 20; ++y) {
+            for(int x = 0; x < 20; ++x) {
+                graphics.drawImage(sprites.get(random.nextInt(30)), x * 32, y * 32, null);
+            }
+        }
+
+    }
+
+    private Color getRandomColor() {
+        return new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
     }
 }
