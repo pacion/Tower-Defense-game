6d8920d5a6548768156c12de011f656b8fda60b7
Patryk <patgut5@o2.pl>
2022-03-18 10:46:35 +0100

    Add updates per second and frames per second.
    Simple game loop with capped and logged fps, ups counter.
    Code clean, window size fixed.

--- src/main/Game.java
+++ src/main/Game.java
@@ -6,27 +6,27 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Game extends JFrame {
-
-    private static int width = 656;
-    private static int height = 646;
+public class Game extends JFrame implements Runnable {
     private GameScreen gameScreen;
     private BufferedImage image;
+    private Thread gameThread;
+    private final double FPS_SET = 120.0;
+    private final double UPS_SET = 60.0;
 
     public Game() {
-
         importImage();
 
-        setSize(width, height);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
         gameScreen = new GameScreen(image);
         add(gameScreen);
+        pack();
+
         setVisible(true);
     }
 
     private void importImage() {
-
         InputStream is = getClass().getResourceAsStream("/res/spriteatlas.png");
 
         try {
@@ -36,7 +36,52 @@ public class Game extends JFrame {
         }
     }
 
+    private void start() {
+        gameThread = new Thread(this) {};
+        gameThread.start();
+    }
+
+    private void updateGame() {
+
+    }
+
     public static void main(String[] args) {
         Game game = new Game();
+        game.start();
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000 * 1000 * 1000.0 / FPS_SET;
+        double timePerUpdate = 1000 * 1000 * 1000.0 / UPS_SET;
+        long lastFrame = System.nanoTime();
+        long lastUpdate = System.nanoTime();
+        long lastTimeCheck = System.currentTimeMillis();
+        int frames = 0;
+        int updates = 0;
+        long now;
+
+        while (true) {
+            now = System.nanoTime();
+
+            if(now - lastFrame >= timePerFrame) {
+                repaint();
+                lastFrame = now;
+                frames++;
+            }
+
+            if(now - lastUpdate >= timePerUpdate) {
+                updateGame();
+                lastUpdate = now;
+                updates++;
+            }
+
+            if(System.currentTimeMillis() - lastTimeCheck >= 1000) {
+                System.out.println("FPS " + frames + " | UPS " + updates);
+                frames = 0;
+                updates = 0;
+                lastTimeCheck = System.currentTimeMillis();
+            }
+        }
     }
 }
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -1,45 +1,52 @@
 package main;
 
 import javax.swing.JPanel;
+import java.awt.Dimension;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class GameScreen extends JPanel {
-
     private Random random;
     private BufferedImage image;
-
+    private Dimension size;
     private ArrayList<BufferedImage> sprites = new ArrayList<>();
 
     public GameScreen(BufferedImage image) {
         this.image = image;
 
+        setPanelSize();
         loadSprites();
 
         random = new Random();
     }
 
-    private void loadSprites() {
+    private void setPanelSize() {
+        size = new Dimension(640, 640);
 
-        for(int y = 0; y < 3; ++y) {
-            for(int x = 0; x < 10; ++x) {
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
+    }
+
+    private void loadSprites() {
+        int rowsOfSpriteatlas = 3;
+        int colsOfSpriteatlas = 10;
+
+        for(int y = 0; y < rowsOfSpriteatlas; ++y)
+            for(int x = 0; x < colsOfSpriteatlas; ++x)
                 sprites.add(image.getSubimage(x * 32, y * 32, 32, 32));
-            }
-        }
     }
 
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
 
-
-        for(int y = 0; y < 20; ++y) {
-            for(int x = 0; x < 20; ++x) {
+        for(int y = 0; y < 20; ++y)
+            for(int x = 0; x < 20; ++x)
                 graphics.drawImage(sprites.get(random.nextInt(30)), x * 32, y * 32, null);
-            }
-        }
 
+        repaint();
     }
 
     private Color getRandomColor() {
