b15bb1a207ce02cbeb7c004cd653349cca95c63e
pacion <patgut5@o2.pl>
2022-07-15 17:12:07 +0200

    Rebase

--- src/objects/Projectile.java
+++ src/objects/Projectile.java
@@ -20,9 +20,9 @@ public class Projectile {
         this.projectileType = projectileType;
     }
 
-    public void move() {
-        position.x += xSpeed;
-        position.y += ySpeed;
+    public void move(float x, float y) {
+        position.x += x;
+        position.y += y;
     }
 
     public Point2D.Float getPosition() {
